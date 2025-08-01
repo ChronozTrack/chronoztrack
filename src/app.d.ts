67ceb2ee425fac291c88014df38071c6f17b880f
ChronozTrack <chornoztrack@gmail.com>
2025-08-02 00:38:29 +0800

    my commit with ssh

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,16 +1,18 @@
 // See https://svelte.dev/docs/kit/types#app.d.ts
 // for information about these interfaces
+// 0
+import { User, Session } from "$lib/app-types";
 declare global {
-	namespace App {
-		interface Locals {
-			user: User | null;
-			session: Session | null;
-		}
-		// interface Error {}
-		// interface PageData {}
-		// interface PageState {}
-		// interface Platform {}
-	}
+  namespace App {
+    interface Locals {
+      user: User | null;
+      session: Session | null;
+    }
+    // interface Error {}
+    // interface PageData {}
+    // interface PageState {}
+    // interface Platform {}
+  }
 }
 
 export { };
