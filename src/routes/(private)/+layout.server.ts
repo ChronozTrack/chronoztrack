49a20f8225a91abb63cd1b2b06d5e799c3043687
ChronozTrack <chornoztrack@gmail.com>
2025-08-02 15:44:00 +0800

    update packages

--- src/routes/(private)/+layout.server.ts
+++ src/routes/(private)/+layout.server.ts
@@ -1,6 +1,4 @@
-import { redirect } from '@sveltejs/kit';
 import type { LayoutServerLoad } from './$types';
-import { APP_PAGES, SETTING_PAGES } from '$lib/defaults/menus';
 
 export const load = (async ({ locals }) => {
   return {
--- src/routes/(private)/+layout.svelte
+++ src/routes/(private)/+layout.svelte
@@ -9,41 +9,42 @@
 	import { APP_PAGES, SETTING_PAGES } from '$lib/defaults/menus';
 
 	let { data, children }: LayoutProps = $props();
-	const permissions = new Set(Object.keys(data.user.permissions));
+	const permissions = new Set(Object.keys(data.user?.permissions ?? {}));
 	const userRoutes = APP_PAGES.filter((route) => permissions.has(route.resource));
 	const settingRoutes = SETTING_PAGES.filter((route) => permissions.has(route.resource));
-	console.log(data);
 </script>
 
-<ModeWatcher />
-<Sidebar.Provider>
-	<AppSidebar {userRoutes} {settingRoutes} user={data.user} />
-	<main>
-		<Sidebar.Inset>
-			<header
-				class="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12"
-			>
-				<div class="flex items-center gap-2 px-4">
-					<Sidebar.Trigger class="-ml-1" />
-					<Separator orientation="vertical" class="mr-2 data-[orientation=vertical]:h-4" />
-					<Breadcrumb.Root>
-						<Breadcrumb.List>
-							<Breadcrumb.Item class="hidden md:block">
-								<Breadcrumb.Link href="#">Building Your Application</Breadcrumb.Link>
-							</Breadcrumb.Item>
-							<Breadcrumb.Separator class="hidden md:block" />
-							<Breadcrumb.Item>
-								<Breadcrumb.Page>Data Fetching</Breadcrumb.Page>
-							</Breadcrumb.Item>
-						</Breadcrumb.List>
-					</Breadcrumb.Root>
+{#if data.user}
+	<ModeWatcher />
+	<Sidebar.Provider>
+		<AppSidebar {userRoutes} {settingRoutes} user={data.user} />
+		<main>
+			<Sidebar.Inset>
+				<header
+					class="flex h-16 shrink-0 items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12"
+				>
+					<div class="flex items-center gap-2 px-4">
+						<Sidebar.Trigger class="-ml-1" />
+						<Separator orientation="vertical" class="mr-2 data-[orientation=vertical]:h-4" />
+						<Breadcrumb.Root>
+							<Breadcrumb.List>
+								<Breadcrumb.Item class="hidden md:block">
+									<Breadcrumb.Link href="#">Building Your Application</Breadcrumb.Link>
+								</Breadcrumb.Item>
+								<Breadcrumb.Separator class="hidden md:block" />
+								<Breadcrumb.Item>
+									<Breadcrumb.Page>Data Fetching</Breadcrumb.Page>
+								</Breadcrumb.Item>
+							</Breadcrumb.List>
+						</Breadcrumb.Root>
+					</div>
+				</header>
+				<div class="flex flex-1 flex-col gap-4 p-4 pt-0">
+					<div class="min-h-[100vh] flex-1 rounded-xl bg-muted/50 md:min-h-min">
+						{@render children?.()}
+					</div>
 				</div>
-			</header>
-			<div class="flex flex-1 flex-col gap-4 p-4 pt-0">
-				<div class="min-h-[100vh] flex-1 rounded-xl bg-muted/50 md:min-h-min">
-					{@render children?.()}
-				</div>
-			</div>
-		</Sidebar.Inset>
-	</main>
-</Sidebar.Provider>
+			</Sidebar.Inset>
+		</main>
+	</Sidebar.Provider>
+{/if}
